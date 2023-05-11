efd33c0c84f18d176e8cd02d65333f0aaf002d86
Andre Dickson <adickson@QF9P9V77RN.local>
2023-05-11 00:03:24 -0400

    second

--- index.js
+++ index.js
@@ -1,82 +1,90 @@
 const fs = require('fs');
+
+let config;
+if(fs.existsSync('evolver-config.json')){
+  const configStream = fs.readFileSync('evolver-config.json', 'utf8');
+  config = JSON.parse(configStream);
+}
+
+// config.files
+// config.changelogPath
+// config.changelogLineStart
+
 const package = JSON.parse(fs.readFileSync('package.json', 'utf8'));
 const replaceInFiles = require('replace-in-files');
 const args = process.argv.slice(2);
-const mode = args[0];
+const mode = args[0] || 'bugfix';
 const modes = ['minor', 'major', 'bugfix'];
 const isValid = modes.some((m)=> m == mode);
 const currentVersion = package.version;
-const cLog = fs.readFileSync('CHANGELOG.md').toString().split("\n");
+
 const lineStart = 8;
+const tempKey = '#EVOLVERTEMPKEY#';
+const changelogPath = 'CHANGELOG.md';
+const versionFiles = [
+  'package.json',
+  'package-lock.json',
+  //'src/environments/*.ts',
+];
 
-cLog.splice(lineStart, 0, "\n");
-cLog.splice(lineStart, 0, "## New");
-cLog.splice(lineStart, 0, "\n");
-
-var cLogNew = cLog.join("\n");
-
-fs.writeFile('CHANGELOG.md', cLogNew, function (err) {
-  if (err) return console.log(err);
-});
-
-let newVersion = '';
-let tempVersion = new String(package.version);
-let parts = tempVersion.split('.');
-
-switch(mode){
-  case 'major': {
-    let maNumber = Number(parts[0])
-    parts[0] = new String(maNumber+1);
-    parts[1] = new String(0);
-    parts[2] = new String(0);
-  }
+function prepChangelog(){
+  const p = new Promise((resolve, reject)=>{
+    const cLog = fs.readFileSync('CHANGELOG.md').toString().split("\n");
+    cLog.splice(lineStart, 0, "\n");
+    cLog.splice(lineStart, 0, tempKey);
+    cLog.splice(lineStart, 0, "\n");
+  
+    const cLogNew = cLog.join("\n");
+  
     
-  break;
-  case 'minor': {
-    let miNumber = Number(parts[1])
-    parts[1] = new String(miNumber+1);
-    parts[2] = new String(0);
-  }
-    
-  break;
-  case 'bugfix': {
-    let bNumber = Number(parts[2])
-    parts[2] = new String(bNumber+1);
-  }    
-  break;
+    fs.writeFile(changelogPath, cLogNew, function (err) {
+      if (err){
+        return console.log(err);
+      } else {
+        resolve();
+      }
+    });
+  });
+  
+  return p;
 }
 
-newVersion = parts.join('.');
+function updateVersion(){
+  let newVersion = '';
+  let tempVersion = new String(package.version);
+  let parts = tempVersion.split('.');
 
-const date = new Date();
-const dateString = date.getFullYear() + '-' + (date.getMonth()+1) + '-' + date.getDate();
-const changeLogValue = '## ['+newVersion+'] - ' + dateString;
+  switch(mode){
+    case 'major': {
+      let maNumber = Number(parts[0])
+      parts[0] = new String(maNumber+1);
+      parts[1] = new String(0);
+      parts[2] = new String(0);
+    }
+      
+    break;
+    case 'minor': {
+      let miNumber = Number(parts[1])
+      parts[1] = new String(miNumber+1);
+      parts[2] = new String(0);
+    }
+      
+    break;
+    case 'bugfix': {
+      let bNumber = Number(parts[2])
+      parts[2] = new String(bNumber+1);
+    }    
+    break;
+  }
 
-console.log(args, currentVersion, isValid, newVersion, changeLogValue);
+  newVersion = parts.join('.');
 
-const changeLogOptions = {
-  files: [
-    'CHANGELOG.MD'
-  ],
-  // See more: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/replace
-  // Replacement
-  from: new RegExp('## New', 'g'),  // string or regex
-  to: changeLogValue, // string or fn  (fn: carrying last argument - path to replaced file)
-};
+  const options = {
+    files: versionFiles,
+    from: new RegExp(currentVersion),  // string or regex
+    to: newVersion, // string or fn  (fn: carrying last argument - path to replaced file)
+  };
 
-const options = {
-  files: [
-    'package.json',
-    'package-lock.json',
-    'src/environments/*.ts',
-  ],
-  // See more: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/replace
-  // Replacement
-  from: new RegExp(currentVersion),  // string or regex
-  to: newVersion, // string or fn  (fn: carrying last argument - path to replaced file)
-};
-
-if(isValid){
   replaceInFiles(options)
     .then(({ changedFiles, countOfMatchesByPaths }) => {
       console.log('Modified files:', changedFiles);
@@ -86,15 +94,46 @@ if(isValid){
     .catch(error => {
       console.error('Error occurred:', error);
     });
-    replaceInFiles(changeLogOptions)
+}
+
+function updateChangelog(){
+  const date = new Date();
+  const dateString = date.getFullYear() + '-' + (date.getMonth()+1) + '-' + date.getDate();
+  const changeLogValue = '## ['+newVersion+'] - ' + dateString;
+
+  const changeLogOptions = {
+    files: [
+      'CHANGELOG.MD'
+    ],
+    // See more: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/replace
+    // Replacement
+    from: new RegExp(tempKey),  // string or regex
+    to: changeLogValue, // string or fn  (fn: carrying last argument - path to replaced file)
+  };
+
+  replaceInFiles(changeLogOptions)
     .then(({ changedFiles, countOfMatchesByPaths }) => {
       console.log('Modified files:', changedFiles);
       console.log('Count of matches by paths:', countOfMatchesByPaths);
-      console.log('was called with:', options);
+      console.log('was called with:', changeLogOptions);
     })
     .catch(error => {
       console.error('Error occurred:', error);
     });
+}
+
+if(isValid){
+  if(fs.existsSync(changelogPath)){
+    prepChangelog().then(()=>{
+      updateVersion();
+      updateChangelog();
+    });
+  } else {
+    updateVersion();
+  }
+  
 } else {
   console.error('Invalid Version Argument', v);
 }
+
+
